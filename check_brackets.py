02f96d7ddb046734a08901707eefcf44466a61c4
Олег Матшин <89175955650@mail.ru>
2022-06-27 23:16:29 +0300

    Function balance_check_2 - realisation with stack.py.

--- check_brackets.py
+++ check_brackets.py
@@ -2,9 +2,10 @@ from stack import Stack
 
 class Brackets:
     def __init__(self):
-        # ( chr(40), ) chr(41), [ chr(91), ] chr(93), { chr(123), } chr(125)
+        # Unicode symbols are: '(' chr(40), ')' chr(41), '[' chr(91), ']' chr(93), '{' chr(123), '}' chr(125)
         self.brackets_collection = f'{chr(40)}{chr(41)}{chr(91)}{chr(93)}{chr(123)}{chr(125)}'
 
+    # String input function
     def inp_string_brackets(self):
         while True:
             string_brackets = input("Please, input string with brackets for testing: ")
@@ -20,38 +21,59 @@ class Brackets:
         list_brackets = list(string_brackets)
         return list_brackets
 
-    def balance_check(self, result="Сбалансированно"):
+    # Realisation without stack.py
+    def balance_check_1(self, result="Сбалансированно"):
         stack_brackets = Brackets()
         list_brackets = stack_brackets.inp_string_brackets()
-        stack_object = Stack(list_brackets)
-        stack_list = stack_object.stack
         while len(list_brackets) != 0:
             try:
                 for symbol in list_brackets:
                     if symbol == chr(40):
                         list_brackets.remove(chr(41))
-                        list_brackets.remove(symbol)
                     elif symbol == chr(91):
                         list_brackets.remove(chr(93))
-                        list_brackets.remove(symbol)
                     elif symbol == chr(123):
                         list_brackets.remove(chr(125))
-                        list_brackets.remove(symbol)
                     elif symbol == chr(41):
                         list_brackets.remove(chr(40))
-                        list_brackets.remove(symbol)
                     elif symbol == chr(93):
                         list_brackets.remove(chr(91))
-                        list_brackets.remove(symbol)
                     elif symbol == chr(125):
                         list_brackets.remove(chr(123))
-                        list_brackets.remove(symbol)
+                    list_brackets.remove(symbol)
             except ValueError:
                 result = "Несбалансированно"
                 break
-        print(result)
+        return result
 
+    # Realisation with stack.py
+    def balance_check_2(self, result="Сбалансированно"):
+        stack_brackets = Brackets()
+        stack_object = Stack(stack_brackets.inp_string_brackets())
+        stack_list = stack_object.stack
+        while stack_object.is_empty() != True:
+            try:
+                for _ in stack_list:
+                    symbol = stack_object.peek()
+                    if symbol == chr(40):
+                        stack_list.remove(chr(41))
+                    elif symbol == chr(91):
+                        stack_list.remove(chr(93))
+                    elif symbol == chr(123):
+                        stack_list.remove(chr(125))
+                    elif symbol == chr(41):
+                        stack_list.remove(chr(40))
+                    elif symbol == chr(93):
+                        stack_list.remove(chr(91))
+                    elif symbol == chr(125):
+                        stack_list.remove(chr(123))
+                    stack_object.pop()
+            except ValueError:
+                result = "Несбалансированно"
+                break
+        return result
 
 
 if __name__ == '__main__':
-    Brackets.balance_check(Brackets)
+    # Brackets.balance_check_1(Brackets) - successfully
+    Brackets.balance_check_2(Brackets)
