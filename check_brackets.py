06b3e6f543bcbee4ec45c5f641c9cc22dbf507a8
Олег Матшин <89175955650@mail.ru>
2022-06-27 12:56:16 +0300

    Rename main.py, stack.py now.

--- check_brackets.py
+++ check_brackets.py
@@ -1,4 +1,4 @@
-import main
+import stack
 
 class Brackets:
     def __init__(self):
@@ -8,19 +8,22 @@ class Brackets:
     def inp_string_brackets(self):
         while True:
             string_brackets = input("Please, input string with brackets for testing: ")
-            count_other_simbol = 0
-            for symbol in string_brackets:
-                if symbol not in self.brackets_collection:
-                    count_other_simbol += 1
-            if count_other_simbol == 0:
-                break
+            count_other_symbol = 0
+            if string_brackets == '':
+                print('No one symbol, try again!')
+            else:
+                for symbol in string_brackets:
+                    if symbol not in self.brackets_collection:
+                        count_other_symbol += 1
+                if count_other_symbol == 0:
+                    break
         return list(string_brackets)
 
     def balance_check(self):
         stack_brackets = Brackets()
         list_brackets = stack_brackets.inp_string_brackets()
-        stack = main.Stack(list_brackets).isEmpty()
-        print(stack)
+        stack_list = stack.Stack(list_brackets).isEmpty()
+        print(stack_list)
 
 
 if __name__ == '__main__':
